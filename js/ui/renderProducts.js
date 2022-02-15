97a3a0dd74ad398e4e59fc5d15b7109a881f2f6f
martineleirvik <leirvikmartine@gmail.com>
2022-02-15 13:29:54 +0100

    renderproducts

--- js/ui/renderProducts.js
+++ js/ui/renderProducts.js
@@ -1,3 +1,5 @@
+import { baseUrl } from "../settings/api.js";
+
 const productsContainer = document.querySelector(".container");
 
 export function renderProducts(render) {
@@ -5,26 +7,12 @@ export function renderProducts(render) {
     productsContainer.innerHTML = "";
 
     render.forEach(function (product){
-        productsContainer.innerHTML += `<a class="product" href="productDetails.html?id=${product.id}">
-        <div class="col-md-6 col-lg-3">
-        <div class="card">
-          <img src="${product.image.formats.large.url}" class="card-img-top" alt="${product.image.alternativeText}">
-          <div class="card-body">
-            <h5 class="card-title">${product.title}</h5>
-            <p class="card-text">${product.price}</p>
-            <a href="productDetails.html?id=${product.id}" class="btn btn-primary">Shop</a>
-          </div>
-        </div>
-      </div>
-        
-        
-        
-        <a class="product" href="productDetails.html?id=${product.id}">
+        productsContainer.innerHTML += `<a class="product-card" href="productDetails.html?id=${product.id}">
                                 <h4>${product.title}</h4>
                                 <p>${product.price}</p>
-                                <img class="heropicture" src="${product.image.formats.large.url}" alt="${product.image.alternativeText}">
-        
-        `
+                                <img class="products-image" src="http://localhost:1337${product.image.formats.medium.url}" alt="${product.image.alternativeText}">
+         
+        </a>`;
 
 
     })
