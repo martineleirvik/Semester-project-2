c765cadd10d7998ace35373283eedcdb67193480
martineleirvik <leirvikmartine@gmail.com>
2022-02-17 11:25:42 +0100

    products page

--- js/ui/renderProducts.js
+++ js/ui/renderProducts.js
@@ -5,16 +5,13 @@ export function renderProducts(render) {
     productsContainer.innerHTML = "";
 
     render.forEach(function (product){
-        productsContainer.innerHTML += `<a class="product-card" href="productDetails.html?id=${product.id}">
-                                <h4>${product.title}</h4>
-                                <p>${product.price}</p>
-                                <img class="products-image" src="http://localhost:1337${product.image.formats.medium.url}" alt="${product.image.alternativeText}">
-         
-        </a>`;
-
-
-    })
-
-
-
+        productsContainer.innerHTML += `<a class="card" href="productDetails.html?id=${product.id}">
+                                            <img class="product-image" src="http://localhost:1337${product.image.formats.medium.url}" alt="${product.image.alternativeText}">
+                                            <div class="product-info">
+                                            <h3>${product.title}</h3>
+                                            <h4>$${product.price}</h4>
+                                            </div>
+                                            <p><button>Add to Cart</button></p>
+                                        </a>`;
+    });
 }
