bc3140fe65fb28809cf0354147eb65ff1015e965
martineleirvik <leirvikmartine@gmail.com>
2022-02-23 13:29:06 +0100

    slider js

--- js/ui/featuredProducts.js
+++ js/ui/featuredProducts.js
@@ -14,3 +14,22 @@ export function getFeaturedProducts(products) {
         </div>`;
     });
 }
+
+const btnRight = document.querySelector(".right-point");
+const btnLeft = document.querySelector(".left-point");
+
+const scrollLeft = Element.scrollLeft;
+
+btnRight.onclick = function () {
+    const slider = document.querySelector(".featured-slide");
+    const item = slider.querySelector(".featured-card");
+    const width = item.offsetWidth;
+    slider.scrollLeft += width;
+};
+
+btnLeft.onclick = function () {
+    const slider = document.querySelector(".featured-slide");
+    const item = slider.querySelector(".featured-card");
+    const width = item.offsetWidth;
+    slider.scrollLeft -= width;
+};
