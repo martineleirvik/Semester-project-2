7ac733d8c8ebeb78100c06d36b07eaef142ca54d
martineleirvik <leirvikmartine@gmail.com>
2022-02-25 15:01:04 +0100

    nav

--- js/components/nav.js
+++ js/components/nav.js
@@ -1,12 +1,10 @@
-const createNav = () => {
+export const createNav = () => {
     const burger = document.querySelector(".burger-menu");
     const nav = document.querySelector(".nav-links");
     
 
     burger.addEventListener("click", () => {
         nav.classList.toggle("nav-active");
-        console.log(nav);
+        console.log(burger);
     });
-
 }
-createNav();
--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+import { createNav } from "./components/nav.js";
 import { displayMessage } from "./components/displayMessage.js";
 import { saveToken, saveUser } from "./utils/storage.js";
 import { baseUrl } from "./settings/api.js";
@@ -9,7 +10,7 @@ const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const message = document.querySelector(".message-container");
 
-
+createNav();
 loginMenu();
 
 form.addEventListener("submit", submitForm);
